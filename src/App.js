c4f07f2b8e4b7798f61995ddf19acedb9e070a8f
rnjsgurcjs12 <ail7890@naver.com>
2021-11-09 11:14:35 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = () => {
+	return (
+		<div className="contenedor">
+			<div className="grupo">
+				<h1>Hola Mundo</h1>
+			</div>
+		</div>
+	);
+}
+ 
+export default App;
